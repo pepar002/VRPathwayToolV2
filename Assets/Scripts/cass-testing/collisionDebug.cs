2c42ffc21a63f22ec7aa47037f93853a309058f8
caykay <kahemacassian@gmail.com>
2022-04-09 18:28:06 +0930

    Basic interactions: highlight and grabbing

---
+++ Assets/Scripts/cass-testing/collisionDebug.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionDebug : MonoBehaviour
+{
+
+    protected NetworkHead[] heads;
+    protected NetworkHead pokingPlayerHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject hand = collision.gameObject.GetComponentInParent<OVRHand>().gameObject;
+        GestureDetector gesture = hand.GetComponentInChildren<GestureDetector>();
+        if (collision.name == "IndexTip" && gesture.isPointing()) { 
+
+        }
+    }
+}
