4e0dec6f5ab80ecb13a60d0da6d66898ee7a2cc1
caykay <kahemacassian@gmail.com>
2022-09-15 11:46:16 +0930

    cube-changes

--- Assets/Scripts/GraphMovement.cs
+++ Assets/Scripts/GraphMovement.cs
@@ -6,13 +6,15 @@ public class GraphMovement : MonoBehaviour
 {
     public GameObject graph;
     public GameObject cube;
-    public bool rotate;
+    private bool rotate;
+
+    public bool Rotate { get => rotate; set => rotate = value; }
 
     // set if graph can be rotated or move along x, y and z plane
     //public bool SetRotate { get => rotate; set => rotate = value; }
     void Start()
     {
-        rotate = true;
+        rotate = false;
     }
 
  
@@ -26,22 +28,24 @@ public class GraphMovement : MonoBehaviour
         {
             if (rotate)
             {
-                UpdateContsraints();
                 graph.transform.rotation = cube.transform.rotation;
             }
             else {
-                UpdateContsraints();
                 graph.transform.position = cube.transform.TransformPoint(2, 2, 1);
             }
         }
     }
 
-    public void UpdateContsraints() {
-        if (rotate)            
-            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
-        else 
-            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    /*    public void UpdateContsraints() {
+            if (rotate)            
+                cube.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            else 
+                cube.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
 
+        }*/
+
+    public void ScaleGraph() { 
+        //
     }
 
 
